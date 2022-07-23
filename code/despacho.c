c6c6c5db0a5f3a6125aff44c814672cbe3edb0a1
Carmofrasao <anderfrasao52@gmail.com>
2022-07-23 03:29:40 -0300

    printf's OK, mas tem umas inequações q usam modulo e eu nao consegui arrumar

--- code/despacho.c
+++ code/despacho.c
@@ -27,13 +27,74 @@ int main(void){
     scanf("%d %f", &t_max, &CT);                        
     scanf("%f", &CA);                                   
 
-    // fazer o for do min
+    printf("/*função objetivo:*/\n");
     printf("min : ");
 
-    printf("%f * (", CT * k);
-    for (int i = 0; i < n; i++) {
-        printf("%f +", );
+    for (int i = 1; i <= n; i++) {
+        printf("CA Ci_%d + CA Cd_%d + ", i, i);
     }
-
+    for (int i = 1; i <= n; i++) {
+        if(i != n)
+            printf("CT gera_t_%d + ", i);
+        else
+            printf("CT gera_t_%d;\n", i);
+    }
+    printf("/*sujeito a:*/\n");
+    printf("/*Constantes:*/\n");
+    printf("CT = %f;\n", CT);
+    printf("CA = %f;\n", CA);
+    printf("v_max = %d;\n", v_max);
+    printf("v_min = %d;\n", v_min);
+    printf("v_ini = %d;\n", v_ini);
+    for(int i = 1; i <= n; i++){
+        printf("d_%d = %d;\n", i, d[i-1]);
+    }
+    printf("k = %f;\n", k);
+    for(int i = 1; i <= n; i++){
+        printf("y_%d = %d;\n", i, y[i-1]);
+    }
+    printf("t_max = %d;\n", t_max);
+    printf("/*Inequações:*/\n");
+    for(int i = 1; i <= n; i++){
+        printf("k gera_h_%d + gera_t_%d >= d_%d;\n", i, i, i);
+    }
+    for (int i = 1; i <= n; i++){
+        printf("gera_h_%d >= 0;\n", i);
+    }
+    for (int i = 1; i <= n; i++){
+        printf("gera_t_%d >= 0;\n", i);
+    }
+    for (int i = 1; i <= n; i++){
+        printf("gera_t_%d <= t_max;\n", i);
+    }
+    printf("v_0 = v_ini;\n");
+    for (int i = 1; i <= n; i++){
+        printf("v_%d = v_%d + C_%d;\n", i, i-1, i);
+    }
+    for (int i = 1; i <= n; i++){
+        printf("v_%d >= v_min;\n", i);
+    }
+    for (int i = 1; i <= n; i++){
+        printf("v_%d <= v_max;\n", i);
+    }
+    // trecho duvidoso
+    for (int i = 1; i <= n; i++){
+        printf("Ci_%d - Cd_%d = y_%d - gera_h_%d;\n", i, i, i, i);
+    }
+    for (int i = 1; i <= n; i++){
+        printf("C_%d = Ci_%d - Cd_%d;\n", i, i, i);
+    }
+    for (int i = 1; i <= n; i++){
+        // for mais duvidoso ainda, em "C_%d" era para ser "|C_%d|", 
+        // mas como representa ||?
+        printf("C_%d = Ci_%d + Cd_%d;\n", i, i, i);
+    }
+    for (int i = 1; i <= n; i++){
+        printf("Ci_%d >= 0;\n", i);
+    }
+    for (int i = 1; i <= n; i++){
+        printf("Cd_%d >= 0;\n", i);
+    }
+    
     return 1;
 }
